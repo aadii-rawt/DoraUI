698fba6fdd14bfe282896653dc4880b8aadcedfa
aadii-rawt <rawatadii060@gmail.com>
2025-04-15 16:57:26 +0530

    create new element

--- client/src/pages/Create.tsx
+++ client/src/pages/Create.tsx
@@ -1,13 +1,85 @@
 import React, { useState } from "react";
 import Editor from "@monaco-editor/react";
 import { FaRegCopy } from "react-icons/fa";
-import { BsMoon } from "react-icons/bs";
+import { RxButton } from "react-icons/rx";
+import { RiToggleLine } from "react-icons/ri";
+import { IoMdCheckboxOutline, IoMdSwitch } from "react-icons/io";
+import { FiLoader } from "react-icons/fi";
+import { IoDocumentOutline } from "react-icons/io5";
 
-const Create = () => {
+type TypeElement = {
+  name: string,
+  value: string,
+  icon: any
+}
+
+const elementsTypes : TypeElement[] = [
+  {
+    name: "Button",
+    value: "button",
+    icon: <RxButton size={30} />
+  },
+  {
+    name: "Toggle",
+    value: "toggle",
+    icon: <RiToggleLine size={30} />
+  },
+  {
+    name: "Switch",
+    value: "switch",
+    icon: <IoMdSwitch size={30} />
+  },
+  {
+    name: "Check Box",
+    value: "checkbox",
+    icon: <IoMdCheckboxOutline size={30} />
+  },
+  {
+    name: "Loader",
+    value: "loader",
+    icon: <FiLoader size={30} />
+  },
+  {
+    name: "Form",
+    value: "form",
+    icon: <IoDocumentOutline size={30} />
+  },
+]
+
+
+type DataType = {
+  id: string,
+  name: string,         // Button, Toggle, etc.
+  type: string,         // 'button', 'toggle', etc.
+  framework: string,    // 'tailwind' | 'css'
+  html: string,         // Raw HTML code
+  css?: string,         // Only for custom CSS (optional)
+  preview: string,      // Rendered preview in iframe
+  bgColor: string,      // Preview background
+  createdAt: Date
+}
+
+const Create: React.FC = () => {
   const [code, setCode] = useState<string>(
     `<button class="button">Button</button>`
   );
   const [bgColor, setBgColor] = useState<string>("#e8e8e8");
+  const [isModalOpen, setIsModalOpen] = useState(true);
+
+  const [formData, setFormdata] = useState({
+    id: crypto.randomUUID(),
+    name: "",
+    type: "",         // 'button', 'toggle', etc.
+    framework: "",    // 'tailwind' | 'css'
+    html: code,         // Raw HTML code  
+    preview: code,      // Rendered preview in iframe
+    bgColor,      // Preview background
+    createdAt: Date
+  })
+
+  const handleType = (item : TypeElement ) : void => {
+    setFormdata((prev) => ({...prev,type: item.name}))
+  }
 
   return (
     <div className="min-h-screen p-6 bg-[#111]">
@@ -78,6 +150,41 @@ const Create = () => {
           Submit for review
         </button>
       </div>
+
+      {isModalOpen && (
+        <div className="fixed inset-0 bg-black bg-opacity-50 z-50 flex items-center justify-center">
+          <div className="bg-[#1a1a1a] rounded-xl p-6 w-[600px] max-w-[90%]">
+            <h2 className="text-white text-2xl font-semibold mb-6 text-center">What are you making?</h2>
+            <div className="grid grid-cols-3 gap-4 text-white">
+              {elementsTypes.map((item, i) => (
+                <button onClick={() => handleType(item)} key={i} className={`p-4 rounded-lg border border-gray-700 hover:border-[#4E46E5] hover:bg-[#2a2a2a] text-center cursor-pointer flex items-center  gap-2 justify-center flex-col ${i === 0 ? 'border-[#4E46E5] bg-[#2a2a2a]' : ''}`}>
+                  {item.icon}
+                  <div className="text-lg">{item.name}</div>
+                </button>
+              ))}
+            </div>
+            <div className="mt-6 flex justify-between items-center">
+              <div className="flex gap-4">
+                <button className="px-3 py-1.5 flex items-center justify-center gap-2 border border-[#4E46E5] text-white rounded-md bg-[#1e1e1e]">
+                  <img src="https://img.icons8.com/fluent/512/css3.png" alt="" className="w-5" />
+                  <span>CSS</span>
+                </button>
+                <button className="px-3 py-1.5 flex items-center justify-center gap-2 border border-[#4E46E5] text-white rounded-md bg-[#1e1e1e]">
+                  <img src="https://static-00.iconduck.com/assets.00/tailwind-css-icon-144x86-czphjb87.png" alt="" className="w-5" />
+                  <span>Tailwind CSS</span>
+                </button>
+              </div>
+              <button
+                onClick={() => setIsModalOpen(false)}
+                className="bg-[#4E46E5] text-white px-3 py-1.5 rounded-md"
+              >
+                Continue
+              </button>
+            </div>
+          </div>
+        </div>
+      )}
+
     </div>
   );
 };
