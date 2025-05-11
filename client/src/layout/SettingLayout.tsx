7435c6f1ecc5071e580f70516c01b4fc8b09f993
aadii-rawt <rawatadii060@gmail.com>
2025-05-11 18:16:28 +0530

    setting profile page

--- client/src/layout/SettingLayout.tsx
+++ client/src/layout/SettingLayout.tsx
@@ -1,4 +1,7 @@
 import React from 'react'
+import { FiUser } from 'react-icons/fi'
+import { MdAlternateEmail } from 'react-icons/md'
+import { TbBrandGoogleAnalytics } from 'react-icons/tb'
 import { Link, NavLink, Outlet } from 'react-router-dom'
 
 const SettingLayout  : React.FC = () => {
@@ -7,9 +10,10 @@ const SettingLayout  : React.FC = () => {
         <div className='px-4 min-w-[250px] border-r-2 border-third '>
             <h1 className='font-semibold text-xl px-5 '>Settings</h1>
             <div className='flex flex-col gap-1 justify-start mt-4'>
-                <NavLink to='/setting/profile' className={(({isActive}) => `${isActive && "bg-third text-white"} text-gray-400 hover:bg-third py-1.5 px-5 rounded font-medium text-[15px] `)}>Profile</NavLink>
-                <NavLink to='/setting/mail' className={(({isActive}) => `${isActive && "bg-third text-white"} text-gray-400 hover:bg-third py-1.5 px-5 rounded font-medium text-[15px] `)}>Email</NavLink>
-                <NavLink to='/setting/stats' className={(({isActive}) => `${isActive && "bg-third text-white"} text-gray-400 hover:bg-third py-1.5 px-5 rounded font-medium text-[15px] `)}>Stats</NavLink>
+                <NavLink to='/setting/profile' className={(({isActive}) => `${isActive && "bg-third text-white"} text-gray-400 hover:bg-third py-1.5 px-5 rounded font-medium text-[15px] flex items-center gap-2`)}><FiUser /> Profile</NavLink>
+                <NavLink to='/setting/mail' className={(({isActive}) => `${isActive && "bg-third text-white"} text-gray-400 hover:bg-third py-1.5 px-5 rounded font-medium text-[15px] flex items-center gap-2`)}> <MdAlternateEmail />Email</NavLink>
+
+                <NavLink to='/setting/stats' className={(({isActive}) => `${isActive && "bg-third text-white"} text-gray-400 hover:bg-third py-1.5 px-5 rounded font-medium text-[15px] flex items-center gap-2`)}><TbBrandGoogleAnalytics /> Stats</NavLink>
             </div>
         </div>
         <Outlet />
--- client/src/pages/settings/ProfileSetting.tsx
+++ client/src/pages/settings/ProfileSetting.tsx
@@ -1,7 +1,75 @@
 import React from 'react'
+import { FiTwitter, FiUser } from 'react-icons/fi'
+import { HiOutlineLocationMarker } from 'react-icons/hi'
+import { LuPencilLine } from 'react-icons/lu'
+import { RiBuilding2Line, RiLinkM } from 'react-icons/ri'
 
 export const ProfileSetting  : React.FC = () => {
   return (
-    <div>this is setting </div>
+    <div className=' w-full'>
+      <h1 className='text-3xl font-semibold'>Personal Information</h1>
+      <p className='text-gray-400 my-2'>This information will be displayed publicly on your profile.</p>
+
+      <div className='mt-5'>
+        <div className='grid grid-cols-2 w-full gap-10'>
+          <div className='flex flex-col gap-0.5 '>
+            <label htmlFor="" className='text-[15px] flex gap-1 items-center'> <FiUser /> Name</label>
+            <input type="text" 
+            className='bg-transparent border-2 border-secondary rounded-md px-3 py-1 text-[15px] outline-none focus-within:border-theme'
+            placeholder='Your name'
+            />
+          </div>
+          <div className='flex flex-col gap-y-1' >
+            <label htmlFor="" className='text-[15px] flex gap-1 items-center'><HiOutlineLocationMarker /> Location</label>
+            <input type="text" 
+            className='bg-transparent border-2 border-secondary rounded-md px-3 py-1 text-[15px] outline-none focus-within:border-theme'
+            placeholder='Your location'
+            />
+          </div>
+        </div>
+
+        <div className='grid grid-cols-2 w-full gap-10 mt-4'>
+          <div className='flex flex-col gap-0.5 '>
+            <label htmlFor="" className='text-[15px] flex gap-1 items-center'> <RiBuilding2Line /> Company</label>
+            <input type="text" 
+            className='bg-transparent border-2 border-secondary rounded-md px-3 py-1 text-[15px] outline-none focus-within:border-theme'
+            placeholder='Your Company'
+            />
+          </div>
+          <div className='flex flex-col gap-y-1' >
+            <label htmlFor="" className='text-[15px] flex gap-1 items-center'> <FiTwitter /> Twitter Handle</label>
+            <input type="text" 
+            className='bg-transparent border-2 border-secondary rounded-md px-3 py-1 text-[15px] outline-none focus-within:border-theme'
+            placeholder='Your Twitter handle (without @)'
+            />
+          </div>
+        </div>
+
+        <div className='grid grid-cols-1 w-full gap-10 mt-4'>
+          <div className='flex flex-col gap-0.5 '>
+            <label htmlFor="" className='text-[15px] flex gap-1 items-center'> <RiLinkM /> Website</label>
+            <input type="text" 
+            className='bg-transparent border-2 border-secondary rounded-md px-3 py-1 text-[15px] outline-none focus-within:border-theme'
+            placeholder='Your website URL'
+            />
+          </div>
+        </div>
+
+        <div className='grid grid-cols-1 w-full gap-10 mt-4'>
+          <div className='flex flex-col gap-0.5 '>
+            <label htmlFor="" className='text-[15px] flex gap-1 items-center'><LuPencilLine /> Bio</label>
+            <textarea  rows={5}
+            className='bg-transparent border-2 border-secondary rounded-md px-3 py-1 text-[15px] outline-none focus-within:border-theme'
+            placeholder='Write a few sentences about yourself'
+            />
+          </div>
+        </div>
+
+        <div className='flex justify-end items-center gap-5 my-5'>
+          <button className='border-2 border-secondary px-3 py-1.5 font-medium rounded-md'>Cancel</button>
+          <button className='bg-theme text-white px-3 py-2 font-medium rounded-md'>Save changes</button>
+        </div>
+      </div>
+    </div>
   )
 }
