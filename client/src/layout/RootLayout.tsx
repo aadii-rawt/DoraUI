e2c695ad3c2700c61b52dfad74eaa557e2248755
aadii-rawt <rawatadii060@gmail.com>
2025-08-05 15:43:27 +0530

    under construction

--- client/src/layout/RootLayout.tsx
+++ client/src/layout/RootLayout.tsx
@@ -9,6 +9,10 @@ const RootLayout: React.FC = () => {
     const { showSigninModal, setShowSigninModal } = useAuthContext()
     return (
         <div className='bg-primary text-white w-full min-h-screen'>
+            <header className='bg-gradient-to-r from-purple-500 to-indigo-500 text-center'>
+                <h1 className='font-medium'>🚧 Site Under Construction 🚧 </h1>
+                <p className='text-sm'>Some features are still being built. </p>
+            </header>
             <Header />
             <Outlet />
             <Footer />
